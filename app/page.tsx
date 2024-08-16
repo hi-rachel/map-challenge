df6a1a34ce688452da8f1deb734d886ea15ee455
hi-rachel <rachel.uiux@gmail.com>
2024-08-16 16:41:43 +0900

    feat: 지도 기본 설정, 마커 타입별 노출/비노출 구현
    - 마커 리스트 조회, 마커 생성
    - 버튼 타입별 마커 노출/비노출

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,172 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const CENTER_LAT = 37.5358994;
+const CENTER_LNG = 126.8969627;
+
+const Home = () => {
+  const [map, setMap] = useState<naver.maps.Map | null>(null);
+  const [markers, setMarkers] = useState<naver.maps.Marker[]>([]);
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([
+    "CAFE",
+    "FOOD",
+    "MART",
+    "PHARMACY",
+  ]);
+
+  const icons = {
+    CAFE: "/pin-cafe.svg",
+    FOOD: "/pin-food.svg",
+    MART: "/pin-mart.svg",
+    PHARMACY: "/pin-pharmacy.svg",
+    MAIN: "/pin-main.svg",
+  };
+
+  useEffect(() => {
+    const initMap = () => {
+      if (window.naver && window.naver.maps) {
+        const mapOptions = {
+          center: new naver.maps.LatLng(CENTER_LAT, CENTER_LNG),
+          zoom: 15,
+          zoomControl: true,
+          zoomControlOptions: {
+            position: naver.maps.Position.TOP_RIGHT,
+          },
+        };
+        const mapInstance = new naver.maps.Map("map", mapOptions);
+        setMap(mapInstance);
+
+        new naver.maps.Circle({
+          map: mapInstance,
+          center: new naver.maps.LatLng(CENTER_LAT, CENTER_LNG),
+          radius: 300,
+          fillColor: "#000000",
+          fillOpacity: 0.2,
+          strokeWeight: 0,
+        });
+
+        new naver.maps.Marker({
+          position: new naver.maps.LatLng(CENTER_LAT, CENTER_LNG),
+          map: mapInstance,
+          icon: {
+            url: icons.MAIN,
+            size: new naver.maps.Size(50, 50),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(20, 40),
+          },
+        });
+      }
+    };
+
+    if (window.naver && window.naver.maps) {
+      initMap();
+    } else {
+      const intervalId = setInterval(() => {
+        if (window.naver && window.naver.maps) {
+          clearInterval(intervalId);
+          initMap();
+        }
+      }, 100);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchMarkers = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API}`);
+        const data = response.data;
+
+        if (map && data.length > 0) {
+          const newMarkers = data.map((item: any) => {
+            const marker = new naver.maps.Marker({
+              position: new naver.maps.LatLng(
+                parseFloat(item.lat),
+                parseFloat(item.lng)
+              ),
+              map: map,
+              title: item.type,
+              icon: {
+                url: icons[item.type as keyof typeof icons],
+                size: new naver.maps.Size(32, 32),
+                origin: new naver.maps.Point(0, 0),
+              },
+            });
+
+            return marker;
+          });
+
+          setMarkers(newMarkers);
+        }
+      } catch (error) {
+        console.error("Error fetching markers:", error);
+      }
+    };
+
+    if (map) {
+      fetchMarkers();
+    }
+  }, [map]);
+
+  useEffect(() => {
+    markers.forEach((marker) => {
+      marker.setVisible(selectedTypes.includes(marker.getTitle()!));
+    });
+  }, [selectedTypes, markers]);
+
+  const toggleType = (type: string) => {
+    setSelectedTypes((prevTypes) =>
+      prevTypes.includes(type)
+        ? prevTypes.filter((t) => t !== type)
+        : [...prevTypes, type]
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <h1>YourPick Map</h1>
+      <div id="map" style={{ width: "100%", height: "500px" }} />
+      <div className="flex justify-center space-x-4 p-4 mt-4">
+        <button
+          className={`flex flex-col items-center ${
+            selectedTypes.includes("CAFE") ? "opacity-100" : "opacity-50"
+          }`}
+          onClick={() => toggleType("CAFE")}
+        >
+          <img src="/pin-cafe.svg" width={32} height={32} alt="카페" />
+          <span>카페</span>
+        </button>
+        <button
+          className={`flex flex-col items-center ${
+            selectedTypes.includes("FOOD") ? "opacity-100" : "opacity-50"
+          }`}
+          onClick={() => toggleType("FOOD")}
+        >
+          <img src="/pin-food.svg" width={32} height={32} alt="음식점" />
+          <span>음식점</span>
+        </button>
+        <button
+          className={`flex flex-col items-center ${
+            selectedTypes.includes("MART") ? "opacity-100" : "opacity-50"
+          }`}
+          onClick={() => toggleType("MART")}
+        >
+          <img src="/pin-mart.svg" width={32} height={32} alt="편의점" />
+          <span>편의점</span>
+        </button>
+        <button
+          className={`flex flex-col items-center ${
+            selectedTypes.includes("PHARMACY") ? "opacity-100" : "opacity-50"
+          }`}
+          onClick={() => toggleType("PHARMACY")}
+        >
+          <img src="/pin-pharmacy.svg" width={32} height={32} alt="약국" />
+          <span>약국</span>
+        </button>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Home;
